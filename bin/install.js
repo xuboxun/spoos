b2059402875ad665da7e6f379b2c452c866b8212
xuboxun <huangyifan0119@163.com>
2019-03-22 15:46:45 +0800

    安装脚本优化

--- bin/install.js
+++ bin/install.js
@@ -1,39 +1,30 @@
 const fs = require('fs');
+const process = require('process');
 const path = require('path');
 const CONF = require('../config');
 
 const app_dir = path.resolve(__dirname, '../');
 
-// TODO: 打印信息及过程优化
 const MESSAGE = {
-    DIR_EXIST: '目录已存在',
-    DIR_NOT_EXIST: '目录尚未创建',
+    LOG_DIR_EXIST: '日志目录已存在',
+    STORE_DIR_EXIST: '存储目录已存在',
 
-    LOG_DIR_CREATE_READY: `创建spoos日志目录，目标路径：${CONF.log_dir}`,
-    STORE_DIR_CREATE_READY: `创建spoos存储目录，目标路径：${CONF.spoos_dir}`,
+    LOG_DIR_CREATE: `创建日志目录，目标路径：${CONF.log_dir}`,
+    STORE_DIR_CREATE: `创建存储目录，目标路径：${CONF.store_dir}`,
 
-    DIR_CREATE_SUCCESS: '目录创建成功',
-    DIR_CREATE_FAILED: '目录创建失败',
+    DIR_CREATE_SUCCESS: '\t-> 目录创建成功',
+    DIR_CREATE_FAILED: '\t-> 目录创建失败',
 
-    INSTALL_SUCCESS: `安装成功，感谢使用: \n\t应用目录：${app_dir}\n\t存储目录：${CONF.spoos_dir}\n\t日志目录：${CONF.log_dir}`,
+    INSTALLED: `检测到已安装应用: \n\t应用目录：${app_dir}\n\t存储目录：${CONF.store_dir}\n\t日志目录：${CONF.log_dir}`,
+    INSTALL_SUCCESS: `安装成功，感谢使用: \n\t应用目录：${app_dir}\n\t存储目录：${CONF.store_dir}\n\t日志目录：${CONF.log_dir}`,
     INSTALL_FAILED: '安装失败，请检查配置文件',
-
-    INSTALLED: `检测到已安装应用: \n\t应用目录：${app_dir}\n\t存储目录：${CONF.spoos_dir}\n\t日志目录：${CONF.log_dir}`,
 };
 
 const dirExist = (dir) => {
-    console.log(`检测目录: ${dir}`);
     try {
         const stat = fs.statSync(dir);
-        if (stat.isDirectory()) {
-            console.log(`\t${MESSAGE.DIR_EXIST}`);
-            return true;
-        } else {
-            console.log(`\t${MESSAGE.DIR_NOT_EXIST}`);
-            return false;
-        }
+        return stat.isDirectory();
     } catch (e) {
-        console.log(`\t${MESSAGE.DIR_NOT_EXIST}`);
         return false;
     }
 }
@@ -43,39 +34,47 @@ const mkDir = (dir) => {
         const res = fs.mkdirSync(dir, {
             recursive: true
         });
-        if (res === undefined) {
-            console.log(MESSAGE.DIR_CREATE_SUCCESS);
-        } else {
-            console.log(MESSAGE.DIR_CREATE_FAILED);
-        }
+        return res === undefined;
     } catch (e) {
-        console.log(MESSAGE.DIR_CREATE_FAILED);
+        return false;
     }
 }
 
 function main() {
-    if (dirExist(CONF.spoos_dir) && dirExist(CONF.log_dir)) {
+    if (dirExist(CONF.store_dir) && dirExist(CONF.log_dir)) {
         console.log(MESSAGE.INSTALLED);
         return true;
     } else {
-        if (!dirExist(CONF.spoos_dir)) {
-            console.log(MESSAGE.DIR_NOT_EXIST);
-            console.log(MESSAGE.STORE_DIR_CREATE_READY);
-            mkDir(CONF.spoos_dir);
+        // 日志目录创建
+        if (dirExist(CONF.log_dir)) {
+            console.log(MESSAGE.LOG_DIR_EXIST);
+        } else {
+            console.log(MESSAGE.LOG_DIR_CREATE);
+            if (mkDir(CONF.log_dir)) {
+                console.log(MESSAGE.DIR_CREATE_SUCCESS)
+            } else {
+                console.log(MESSAGE.DIR_CREATE_FAILED);
+            }
         }
-        if (!dirExist(CONF.log_dir) {
-            console.log(MESSAGE.DIR_NOT_EXIST);
-            console.log(MESSAGE.LOG_DIR_CREATE_READY);
-            mkDir(CONF.log_dir);
+
+        // 存储目录创建
+        if (dirExist(CONF.store_dir)) {
+            console.log(MESSAGE.STORE_DIR_EXIST);
+        } else {
+            console.log(MESSAGE.STORE_DIR_CREATE);
+            if (mkDir(CONF.store_dir)) {
+                console.log(MESSAGE.DIR_CREATE_SUCCESS);
+            } else {
+                console.log(MESSAGE.DIR_CREATE_FAILED);
+            }
         }
     }
 
-    if (dirExist(CONF.spoos_dir) && dirExist(CONF.log_dir)) {
+    if (dirExist(CONF.store_dir) && dirExist(CONF.log_dir)) {
         console.log(MESSAGE.INSTALL_SUCCESS);
     } else {
         console.log(MESSAGE.INSTALL_FAILED);
     }
-
 }
 
 main();
